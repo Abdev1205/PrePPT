7f671862db6602579f0311b52ddb9e33cc669a9c
Abhay1205Mishra <abhaym1205@gmail.com>
2023-11-05 18:51:55 +0530

    solved backend api probelm adn in frontend solved javascript api calling problem --dev_abhay cuurent problem api is wrong

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import cors from "cors";
 
 const result = dotenv.config();
 const openai = new OpenAI({
-  apiKey: process.env.API_KEY,
+    apiKey: process.env.API_KEY,
 });
 
 const app = express();
@@ -15,66 +15,28 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", async ( req , res )=>{
-
-    const {message} = req.body;
+app.post("/", async (req, res) => {
+    const { message } = req.body;
 
     const completion = await openai.chat.completions.create({
-        model:"gpt-3.5-turbo",
-        messages:[
+        model: "gpt-3.5-turbo",
+        messages: [
             {
-                role:"system", content:"You are PrePPT a helping assitant provides content slides for the PPT ,a PPT generater",
-            }
-        ]
-    })
+                role: "system",
+                content: "You are PrePPT, a helping assistant that provides content slides for the PPT, a PPT generator.",
+            },
+            {
+                role: "user",
+                content: message,
+            },
+        ],
+    });
+
     res.json({
-        completion: completion.choices
-    })
+        completion: completion.choices[0],
+    });
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
-
-    
-
-
-
-
-
-
-
-
-
-
-// import {config} from "dotenv"
-// config()
-// console.log(process.env.API_KEY)
-
-
-
-// import {Configuration , OpenAIApi } from "openai"
-// const openai = new OpenAIApi(new Configuration({
-//     apiKey: process.env.API_KEY ,
-// }))
-
-// const completion = await openai.createCompletion({
-//     model:"gpt-3-turbo",
-//     messages:[
-//         {
-//             role:"user", content: "Hello World",
-//         }
-//     ]
-// }).then(res=>{
-//     console.log(res.data.choices)
-// })
-
-// console.log(completion.data.choices[0].messages);
-
-// const configuration = new Configuration({
-//     organization: "org-cICJIZgpjngAJr61FUmtGnzG",
-//     apiKey:'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',
-// });
-
-
-
